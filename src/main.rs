8827c206265c0a2bccb57c65c11d1543f18fad66
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2018-01-03 00:15:33 +0100

    Actually fix?

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-#![feature(nll)]
-#![feature(slice_patterns)]
-#![feature(conservative_impl_trait)]
-
 extern crate failure;
 extern crate reqwest;
 extern crate rss;
@@ -39,73 +35,74 @@ fn check_prods(options: &Opt) -> Result<(), Error> {
     let pouet_api_client = prodbot::PouetAPIClient::new();
 
     for prod_id in &options.pouet_prod_ids {
-        if let Ok(prod_response) = pouet_api_client.get_prod(*prod_id) {
-            let cache_key = &format!("cache/{}.json", prod_id);
+        let prod_response = pouet_api_client.get_prod(*prod_id)?;
 
-            let mut vote_diff = 0;
-            let mut cached_prod_response: Option<prodbot::ProdResponse> = None;
-            if let Ok(file) = File::open(cache_key) {
-                let shadowed_cached_prod_response: prodbot::ProdResponse =
-                    serde_json::from_reader(file)?;
+        let cache_key = &format!("cache/{}.json", prod_id);
 
-                vote_diff = prod_response.prod.vote_count()
-                    - shadowed_cached_prod_response.prod.vote_count();
-                if vote_diff == 0 {
-                    println!("Prod {} has no difference between pouet and cache. Skipping webhook delivery",
-                                 prod_response.prod.name);
-                    continue;
-                }
+        let mut vote_diff = 0;
+        let mut cached_prod_response: Option<prodbot::ProdResponse> = None;
+        if let Ok(file) = File::open(cache_key) {
+            let shadowed_cached_prod_response: prodbot::ProdResponse =
+                serde_json::from_reader(file)?;
 
-                cached_prod_response = Some(shadowed_cached_prod_response);
+            let prod_count = prod_response.prod.vote_count();
+            let cached_prod_count = shadowed_cached_prod_response.prod.vote_count();
+            vote_diff = prod_count - cached_prod_count;
+            if vote_diff == 0 {
+                println!("Prod {} has no difference between pouet and cache ({} versus {} votes). Skipping webhook delivery",
+                         prod_response.prod.name, prod_count, cached_prod_count);
+                continue;
             }
 
-            let client = pouet_api_client.get_comments(*prod_id)?;
-            let comments_text = client
-                .items()
-                .iter()
-                .take(vote_diff)
-                .map(|comment| {
-                    format!(
-                        "\n<{}|{}> [{}] {}",
-                        comment.link().unwrap_or(""),
-                        comment.title().unwrap_or(""),
-                        comment
-                            .extensions()
-                            .get("pouet")
-                            .and_then(|m| m.get("vote"))
-                            .and_then(|e| e[0].value())
-                            .unwrap_or(""),
-                        comment.description().unwrap_or("")
-                    )
-                })
-                .collect::<String>();
-
-            let postfix = cached_prod_response
-                .map_or("[no cached value]".to_string(), |response| {
-                    response.prod.vote_string()
-                });
-            let slack_text = format!(
-                "Prod <https://www.pouet.net/prod.php?which={}|{}> now has {} up from {}\n{}",
-                prod_id,
-                prod_response.prod.name,
-                prod_response.prod.vote_string(),
-                postfix,
-                comments_text,
-            );
-
-            println!("{}", slack_text);
-            if let Some(ref slack_webhook_url) = options.slack_webhook_url {
-                reqwest_client
-                    .post(slack_webhook_url)
-                    .json(&json!({
-                            "text": slack_text,
-                        }))
-                    .send()?;
-                println!("Delivered slack webhook");
-            }
-
-            serde_json::to_writer(File::create(cache_key)?, &prod_response)?;
+            cached_prod_response = Some(shadowed_cached_prod_response);
         }
+
+        let client = pouet_api_client.get_comments(*prod_id)?;
+        let comments_text = client
+            .items()
+            .iter()
+            .take(vote_diff)
+            .map(|comment| {
+                format!(
+                    "\n<{}|{}> [{}] {}",
+                    comment.link().unwrap_or(""),
+                    comment.title().unwrap_or(""),
+                    comment
+                        .extensions()
+                        .get("pouet")
+                        .and_then(|m| m.get("vote"))
+                        .and_then(|e| e[0].value())
+                        .unwrap_or(""),
+                    comment.description().unwrap_or("")
+                )
+            })
+            .collect::<String>();
+
+        let postfix = cached_prod_response
+            .map_or("[no cached value]".to_string(), |response| {
+                response.prod.vote_string()
+            });
+        let slack_text = format!(
+            "Prod <https://www.pouet.net/prod.php?which={}|{}> now has {} up from {}\n{}",
+            prod_id,
+            prod_response.prod.name,
+            prod_response.prod.vote_string(),
+            postfix,
+            comments_text,
+        );
+
+        println!("{}", slack_text);
+        if let Some(ref slack_webhook_url) = options.slack_webhook_url {
+            reqwest_client
+                .post(slack_webhook_url)
+                .json(&json!({
+                        "text": slack_text,
+                    }))
+                .send()?;
+            println!("Delivered slack webhook");
+        }
+
+        serde_json::to_writer(File::create(cache_key)?, &prod_response)?;
     }
 
     Ok(())
